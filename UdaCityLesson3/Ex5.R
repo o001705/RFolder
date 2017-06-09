935cc72e57b480db94419a6098de082bf4b1566f
o001705 <rkappagantu@gmail.com>
2017-06-09 23:07:30 +0530

    updated ex5

--- UdaCityLesson3/Ex5.R
+++ UdaCityLesson3/Ex5.R
@@ -4,3 +4,9 @@ if (require("ggplot2") == FALSE) {
 }
 
 data(diamonds)
+
+summary(diamonds$price)
+
+qplot(diamonds$price, xlim=c(300, 18900), 
+      ylim=c(0, 5500), binwidth=1000,
+      colour=I("blue"), fill=I("pink"))
